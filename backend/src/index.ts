8faa87bca15aa05da280e1cc0378bc2881bf9011
Abhikansh Mittal <abhikansh33@gmail.com>
2026-01-14 12:54:31 +0530

    intialise

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+
+import express from 'express'
+import { WebSocketServer, WebSocket } from 'ws'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+const httpServer = app.listen(8080)
+
+const wss = new WebSocketServer({ server: httpServer })
+
+wss.on('connection', function connection(ws) {
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary })
+      }
+    })
+  })
+
+  ws.send('message from server')
+})
