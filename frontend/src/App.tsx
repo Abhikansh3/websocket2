f702031da73217d186131f771716bc12d521fe3c
Abhikansh Mittal <abhikansh33@gmail.com>
2026-01-15 15:04:32 +0530

    frontend connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+  const [lastestMessage, setLaststMessage] = useState([])
+  const [message, setMessage] = useState("")
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080')
+    socket.onopen = () => {
+      console.log('connected')
+    }
 
+    socket.onmessage = (message) => {
+      console.log('received:', message.data)
+      setLaststMessage(message.data)
+    }
+    setSocket(socket)
+  }, [])
+
+
+  if (!socket) {
+    return (
+      <div>
+        web socker loading ....
+      </div>
+    )
+  }
   return (
     <>
-      hi there
+      <input onChange={(e) => {
+        setMessage(e.target.value)
+      }} />
+      <button onClick={() => {
+        socket.send(message)
+      }}></button>
+
+      {lastestMessage}
     </>
   )
 }
